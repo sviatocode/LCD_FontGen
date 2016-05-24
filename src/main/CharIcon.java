9cd2b18597013eff90f2bb36141b748d1a7c914c
Sviatoslav Semchyshyn <fpgamcu@gmail.com>
2016-05-24 07:34:59 +0300

    Edit CharIcon class

--- src/main/CharIcon.java
+++ src/main/CharIcon.java
@@ -9,9 +9,12 @@ package main;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.EventQueue;
 import java.awt.Graphics;
 
 import javax.swing.Icon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class CharIcon implements Icon {
 
@@ -55,6 +58,31 @@ public class CharIcon implements Icon {
                 g.fillRect(x + xi * m, y + yi * m, m, m);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    JFrame frame = new JFrame("CharIcon class test");
+                    CharIcon icon = new CharIcon(10, 8, 10);
+                    JLabel label = new JLabel();
+                    icon.setPixel(0, 0, true);
+                    icon.setPixel(0, 1, true);
+                    icon.setPixel(0, 2, true);
+                    icon.setPixel(0, 4, true);
+                    icon.setPixel(1, 1, true);
+                    icon.setPixel(2, 2, true);
+                    icon.setPixel(3, 3, true);
+                    label.setIcon(icon);
+                    frame.add(label);
+                    frame.pack();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 }
